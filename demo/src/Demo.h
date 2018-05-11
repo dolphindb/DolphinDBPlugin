b661d50fcc374f4eefc98a44db8fa81a3e64b54f
dolphindb <36416315+dolphindb@users.noreply.github.com>
2018-05-11 10:41:20 -0400

    Add files via upload
    added demo module

---
+++ demo/src/Demo.h
@@ -0,0 +1,16 @@
+/*
+ * Demo.h
+ *
+ *  Created on: Apr 19, 2017
+ *      Author: dzhou
+ */
+
+#ifndef DEMO_H_
+#define DEMO_H_
+
+#include "CoreConcept.h"
+
+extern "C" ConstantSP minmax(const ConstantSP& a, const ConstantSP& b);
+extern "C" ConstantSP echo(Heap* heap, vector<ConstantSP>& arguments );
+
+#endif /* DEMO_H_ */
