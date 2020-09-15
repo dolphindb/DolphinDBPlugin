ba83cead61bcb4d30db6243b4d306618154867fd
xiaodanzhang <xiaodanzhang@users.noreply.github.com>
2020-09-15 10:04:12 -0400

    Add files via upload

--- httpClient/src/httpClient.h
+++ httpClient/src/httpClient.h
@@ -1,17 +1,10 @@
 #include <CoreConcept.h>
 
-#ifdef WINDOWS
-    #define EXPORT_DLL __declspec(dllexport)
-#else
-    #define EXPORT_DLL
-#endif
-
-extern "C" ConstantSP EXPORT_DLL httpGet(Heap *heap, vector<ConstantSP> &args);
-extern "C" ConstantSP EXPORT_DLL httpPost(Heap *heap, vector<ConstantSP> &args);
+extern "C" ConstantSP httpGet(Heap *heap, vector<ConstantSP> &args);
+extern "C" ConstantSP httpPost(Heap *heap, vector<ConstantSP> &args);
 
 namespace httpClient {
 enum RequestMethod { GET, POST };
-ConstantSP httpRequest(RequestMethod method, const ConstantSP &url,
-                       const ConstantSP &params, const ConstantSP &timeout,
-                       const ConstantSP &nobody);
+ConstantSP httpRequest(RequestMethod method,
+                       const ConstantSP &url, const ConstantSP &params, const ConstantSP &timeout,const ConstantSP &headers);
 }
