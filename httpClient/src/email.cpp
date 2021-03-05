4d27d420e3d5b26f283adcf09938faa9e7a841a8
Xiaodan <xiaodan.zhang@dolphindb.com>
2021-03-05 18:28:57 -0500

    update httpClient

--- httpClient/src/email.cpp
+++ httpClient/src/email.cpp
@@ -11,67 +11,62 @@
 
 unordered_map<std::string, std::string> smtpHost;
 unordered_map<std::string, int> smtpPost;
-bool firstConfigSmtp=true;
+bool firstConfigSmtp = true;
 
 size_t curlWriteData(void *ptr, size_t size, size_t nmemb, string *data) {
-    data->append((char *)ptr, size * nmemb);
+    data->append((char *) ptr, size * nmemb);
     return size * nmemb;
 }
 
-CSendMail::CSendMail( 
-                const std::string & strUser,
-                const std::string & strPsw
-            )
-{
+CSendMail::CSendMail(
+        const std::string &strUser,
+        const std::string &strPsw
+) {
     m_strUser = strUser;
     m_strPsw = strPsw;
     m_RecipientList.clear();
     m_MailContent.clear();
     m_iMailContentPos = 0;
-    if(firstConfigSmtp){
-        smtpHost["126.com"]="smtp.126.com";
-        smtpHost["163.com"]="smtp.163.com";
-        smtpHost["yeah.net"]="smtp.yeah.net";
-        smtpHost["sohu.com"]="smtp.sohu.com";
-        smtpHost["dolphindb.com"]="smtp.ionos.com";
-        smtpHost["qq.com"]="smtp.qq.com";
-        firstConfigSmtp=false;
+    if (firstConfigSmtp) {
+        smtpHost["126.com"] = "smtp.126.com";
+        smtpHost["163.com"] = "smtp.163.com";
+        smtpHost["yeah.net"] = "smtp.yeah.net";
+        smtpHost["sohu.com"] = "smtp.sohu.com";
+        smtpHost["dolphindb.com"] = "smtp.ionos.com";
+        smtpHost["qq.com"] = "smtp.qq.com";
+        firstConfigSmtp = false;
     }
 }
 
-size_t CSendMail::read_callback(void* ptr, size_t size, size_t nmemb, void* userp)
-{
-    CSendMail * pSm = (CSendMail *)userp;
+size_t CSendMail::read_callback(void *ptr, size_t size, size_t nmemb, void *userp) {
+    CSendMail *pSm = (CSendMail *) userp;
 
-    if(size*nmemb < 1)
+    if (size * nmemb < 1)
         return 0;
-    if(pSm->m_iMailContentPos < (int)pSm->m_MailContent.size())
-    {
+    if (pSm->m_iMailContentPos < (int) pSm->m_MailContent.size()) {
         size_t len;
         len = pSm->m_MailContent[pSm->m_iMailContentPos].length();
 
-        memcpy(ptr, pSm->m_MailContent[pSm->m_iMailContentPos].c_str(), pSm->m_MailContent[pSm->m_iMailContentPos].length());
-        pSm->m_iMailContentPos++; 
+        memcpy(ptr, pSm->m_MailContent[pSm->m_iMailContentPos].c_str(),
+               pSm->m_MailContent[pSm->m_iMailContentPos].length());
+        pSm->m_iMailContentPos++;
         return len;
     }
     return 0;
 }
 
-bool CSendMail::ConstructHead(const std::string & strSubject, const std::string & strContent)
-{
+bool CSendMail::ConstructHead(const std::string &strSubject, const std::string &strContent) {
     m_MailContent.push_back("MIME-Versioin: 1.0\n");
     std::string strTemp = "To: ";
-    for(std::list<std::string >::iterator it = m_RecipientList.begin(); it != m_RecipientList.end();)
-    {
+    for (std::list<std::string>::iterator it = m_RecipientList.begin(); it != m_RecipientList.end();) {
         strTemp += *it;
         it++;
-        if(it != m_RecipientList.end())
-                strTemp += ",";
-    }  
+        if (it != m_RecipientList.end())
+            strTemp += ",";
+    }
     strTemp += "\n";
     m_MailContent.push_back(strTemp);
-    if(strSubject != "")
-    {
+    if (strSubject != "") {
         strTemp = "Subject: ";
         strTemp += strSubject;
         strTemp += "\n";
@@ -79,67 +74,63 @@ bool CSendMail::ConstructHead(const std::string & strSubject, const std::string 
     }
     m_MailContent.push_back("Content-Transfer-Encoding: 8bit\n");
     m_MailContent.push_back("Content-Type: text/html; \n Charset=\"UTF-8\"\n\n");
-    if(strContent != "")
-    {
+    if (strContent != "") {
         m_MailContent.push_back(strContent);
     }
-   
+
     return true;
 }
 
-ConstantSP CSendMail::SendMail(const std::string& strSubject, const std::string& strMailBody)
-{
+ConstantSP CSendMail::SendMail(const std::string &strSubject, const std::string &strMailBody) {
     ConstantSP res = Util::createDictionary(DT_STRING, nullptr, DT_ANY, nullptr);
     char errorBuf[CURL_ERROR_SIZE];
     m_MailContent.clear();
     m_iMailContentPos = 0;
     ConstructHead(strSubject, strMailBody);
     CURL *curl;
-    struct curl_slist* rcpt_list = NULL;
+    struct curl_slist *rcpt_list = NULL;
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
     curl = curl_easy_init();
-    if (!curl)
-    {
+    if (!curl) {
         curl_easy_cleanup(curl);
         curl_global_cleanup();
         throw IllegalArgumentException(__FUNCTION__, "Init curl failed");
         return res;
     }
     std::string strListMailTo;
-    for(std::list<std::string >::iterator it = m_RecipientList.begin(); it != m_RecipientList.end();it++)
-    {
-        strListMailTo+=it->c_str();
-        strListMailTo+=' ';
+    for (std::list<std::string>::iterator it = m_RecipientList.begin(); it != m_RecipientList.end(); it++) {
+        strListMailTo += it->c_str();
+        strListMailTo += ' ';
     }
-    for(std::list<std::string >::iterator it = m_RecipientList.begin(); it != m_RecipientList.end();it++)
-    {
+    for (std::list<std::string>::iterator it = m_RecipientList.begin(); it != m_RecipientList.end(); it++) {
         rcpt_list = curl_slist_append(rcpt_list, it->c_str());
-    } 
+    }
     string strSmtpServer;
-    int port=25;
-    int pos=m_strUser.find_first_of('@');
-    string tmp=m_strUser.substr(pos+1);
-    if(smtpHost.count(tmp)!=0)
-        strSmtpServer=smtpHost[tmp];
-    else{
+    int port = 25;
+    int pos = m_strUser.find_first_of('@');
+    string tmp = m_strUser.substr(pos + 1);
+    if (smtpHost.count(tmp) != 0)
+        strSmtpServer = smtpHost[tmp];
+    else {
         curl_slist_free_all(rcpt_list);
         curl_easy_cleanup(curl);
         curl_global_cleanup();
-        throw IllegalArgumentException(__FUNCTION__, "Enter @126.com,@163.com,@yeah.net,@sohu.com,@dolphindb.com,@qq.com or config the email smtpServer");
+        throw IllegalArgumentException(__FUNCTION__,
+                                       "Enter @126.com,@163.com,@yeah.net,@sohu.com,@dolphindb.com,@qq.com or config the email smtpServer");
     }
-    if(smtpPost.count(tmp)!=0)
-        port=smtpPost[tmp];
+    if (smtpPost.count(tmp) != 0)
+        port = smtpPost[tmp];
 
     std::string strUrl = "smtp://" + strSmtpServer;
     strUrl += ":";
     strUrl += std::to_string(port);
     curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuf);
     curl_easy_setopt(curl, CURLOPT_URL, strUrl.c_str());
-    curl_easy_setopt(curl, CURLOPT_USERNAME,  m_strUser.c_str());
-    curl_easy_setopt(curl, CURLOPT_PASSWORD,  m_strPsw.c_str());
+    curl_easy_setopt(curl, CURLOPT_USERNAME, m_strUser.c_str());
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, m_strPsw.c_str());
     curl_easy_setopt(curl, CURLOPT_READFUNCTION, &read_callback);
-    curl_easy_setopt(curl, CURLOPT_MAIL_FROM,m_strUser.c_str());
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, m_strUser.c_str());
     curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, rcpt_list);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
@@ -147,7 +138,7 @@ ConstantSP CSendMail::SendMail(const std::string& strSubject, const std::string&
     curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
     curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
     curl_easy_setopt(curl, CURLOPT_SSLVERSION, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_SESSIONID_CACHE, 0L);    
+    curl_easy_setopt(curl, CURLOPT_SSL_SESSIONID_CACHE, 0L);
     string responseString;
     string headerString;
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteData);
@@ -164,10 +155,10 @@ ConstantSP CSendMail::SendMail(const std::string& strSubject, const std::string&
         curl_slist_free_all(rcpt_list);
         curl_easy_cleanup(curl);
         curl_global_cleanup();
-        if(responseCode==535)
+        if (responseCode == 535)
             throw RuntimeException("UserId or password is incorrect");
-        else 
-             throw RuntimeException("Failed to send the email");
+        else
+            throw RuntimeException("Failed to send the email");
     }
 
     res->set(new String("responseCode"), new Int(responseCode));
@@ -182,62 +173,58 @@ ConstantSP CSendMail::SendMail(const std::string& strSubject, const std::string&
     return res;
 }
 
-ConstantSP sendEmail(Heap *heap, vector<ConstantSP> &args)
-{
-    ConstantSP userId,pwd;
-    ConstantSP recipient,subject,body;
-    userId=args[0];
+ConstantSP sendEmail(Heap *heap, vector<ConstantSP> &args) {
+    ConstantSP userId, pwd;
+    ConstantSP recipient, subject, body;
+    userId = args[0];
     if (userId->getType() != DT_STRING || userId->getForm() != DF_SCALAR)
         throw IllegalArgumentException(__FUNCTION__, "UserId must be a string scalar");
-    pwd=args[1];
+    pwd = args[1];
     if (pwd->getType() != DT_STRING || pwd->getForm() != DF_SCALAR)
         throw IllegalArgumentException(__FUNCTION__, "Pwd must be a string scalar");
-    recipient=args[2];
-    if ((recipient->getType() != DT_STRING || recipient->getForm() != DF_SCALAR)&&
-    (recipient->getType() != DT_STRING || recipient->getForm() != DF_VECTOR))
+    recipient = args[2];
+    if ((recipient->getType() != DT_STRING || recipient->getForm() != DF_SCALAR) &&
+        (recipient->getType() != DT_STRING || recipient->getForm() != DF_VECTOR))
         throw IllegalArgumentException(__FUNCTION__, "recipient must be a string scalar or a string vector");
-    subject=args[3];
+    subject = args[3];
     if (subject->getType() != DT_STRING || subject->getForm() != DF_SCALAR)
         throw IllegalArgumentException(__FUNCTION__, "Suject must be a string scalar");
-    body=args[4];
+    body = args[4];
     if (body->getType() != DT_STRING || body->getForm() != DF_SCALAR)
         throw IllegalArgumentException(__FUNCTION__, "Body must be a string scalar");
-    string strinUser=userId->getString(),strinPsw=pwd->getString();
-    string strinSubject=subject->getString(),strinContext=body->getString();
-    CSendMail mail(strinUser,strinPsw);
-    if(recipient->getForm()==DF_VECTOR)
-    {
-        Vector* veMailTo=((VectorSP)recipient).get();
-        for(int i=0;i<veMailTo->size();++i)
-        {
+    string strinUser = userId->getString(), strinPsw = pwd->getString();
+    string strinSubject = subject->getString(), strinContext = body->getString();
+    CSendMail mail(strinUser, strinPsw);
+    if (recipient->getForm() == DF_VECTOR) {
+        Vector *veMailTo = ((VectorSP) recipient).get();
+        for (int i = 0; i < veMailTo->size(); ++i) {
             mail.AddRecipient(veMailTo->get(i)->getString());
         }
-    }
-    else 
+    } else
         mail.AddRecipient(recipient->getString());
-    ConstantSP res=mail.SendMail(strinSubject,strinContext);
+    ConstantSP res = mail.SendMail(strinSubject, strinContext);
     return res;
 }
 
 
-ConstantSP emailSmtpConfig(Heap *heap, vector<ConstantSP> &args){
-    if(args[0]->getType()!=DT_STRING||args[0]->getForm()!=DF_SCALAR){
+ConstantSP emailSmtpConfig(Heap *heap, vector<ConstantSP> &args) {
+    if (args[0]->getType() != DT_STRING || args[0]->getForm() != DF_SCALAR) {
         throw IllegalArgumentException(__FUNCTION__, "EmailName must be a string scalar");
     }
-    if(args[1]->getType()!=DT_STRING||args[1]->getForm()!=DF_SCALAR){
+    if (args[1]->getType() != DT_STRING || args[1]->getForm() != DF_SCALAR) {
         throw IllegalArgumentException(__FUNCTION__, "Host must be a string scalar");
     }
-    if(args.size()==3){
-        if(args[2]->getType()!=DT_INT||args[2]->getForm()!=DF_SCALAR){
+    if (args.size() == 3) {
+        if (args[2]->getType() != DT_INT || args[2]->getForm() != DF_SCALAR) {
             throw IllegalArgumentException(__FUNCTION__, "Post must be a int scalar");
         }
     }
-    if(args[0]->getString()=="")
+    if (args[0]->getString() == "")
         throw IllegalArgumentException(__FUNCTION__, "EmailName can't be empty");
-    if(args[1]->getString()=="")
+    if (args[1]->getString() == "")
         throw IllegalArgumentException(__FUNCTION__, "Host can't be empty");
-    smtpHost[args[0]->getString()]=args[1]->getString();
-    if(args.size()==3)
-        smtpPost[args[0]->getString()]=args[2]->getInt();
+    smtpHost[args[0]->getString()] = args[1]->getString();
+    if (args.size() == 3)
+        smtpPost[args[0]->getString()] = args[2]->getInt();
     return new Bool(true);
 }
--- httpClient/src/httpClient.cpp
+++ httpClient/src/httpClient.cpp
@@ -8,194 +8,193 @@
 
 ConstantSP httpGet(Heap *heap, vector<ConstantSP> &args) {
     ConstantSP url = args[0];
-    ConstantSP params, timeout,headers;
+    ConstantSP params, timeout, headers;
 
     if (url->getType() != DT_STRING || url->getForm() != DF_SCALAR)
-        throw IllegalArgumentException(__FUNCTION__, "url must be a string scalar");
+        throw IllegalArgumentException(__FUNCTION__, "Url must be a string scalar");
     if (args.size() >= 2 && !args[1]->isNull()) {
         params = args[1];
         if ((params->getType() != DT_STRING || params->getForm() != DF_SCALAR) &&
             (params->getForm() != DF_DICTIONARY ||
-            ((DictionarySP)params)->getKeyType() != DT_STRING ||
-            ((DictionarySP)params)->getType() != DT_STRING))
-            throw IllegalArgumentException(__FUNCTION__, "params must be a string or a dictionary with STRING-STRING key-value type");
-    }
-    else
+             ((DictionarySP) params)->getKeyType() != DT_STRING ||
+             ((DictionarySP) params)->getType() != DT_STRING))
+            throw IllegalArgumentException(__FUNCTION__,
+                                           "Params must be a string or a dictionary with STRING-STRING key-value type");
+    } else
         params = new String("");
     if (args.size() >= 3) {
         timeout = args[2];
         if (!timeout->isNumber() || timeout->getForm() != DF_SCALAR)
-            throw IllegalArgumentException(__FUNCTION__, "timeout must be an integer");
-    }
-    else
+            throw IllegalArgumentException(__FUNCTION__, "Timeout must be an integer scalar");
+    } else
         timeout = new Int(0);
     if (args.size() >= 4 && !args[3]->isNull()) {
         headers = args[3];
         if ((headers->getType() != DT_STRING || headers->getForm() != DF_SCALAR) &&
             (headers->getForm() != DF_DICTIONARY ||
-            ((DictionarySP)headers)->getKeyType() != DT_STRING ||
-            ((DictionarySP)headers)->getType() != DT_STRING))
-            throw IllegalArgumentException(__FUNCTION__, "headers must be a string or a dictionary with STRING-STRING key-value type");
-    }
-    else
+             ((DictionarySP) headers)->getKeyType() != DT_STRING ||
+             ((DictionarySP) headers)->getType() != DT_STRING))
+            throw IllegalArgumentException(__FUNCTION__,
+                                           "Headers must be a string or a dictionary with STRING-STRING key-value type");
+    } else
         headers = new String("");
-    return httpClient::httpRequest(httpClient::GET, url, params, timeout,headers);
+    return httpClient::httpRequest(httpClient::GET, url, params, timeout, headers);
 }
 
 ConstantSP httpPost(Heap *heap, vector<ConstantSP> &args) {
     ConstantSP url = args[0];
-    ConstantSP params, timeout,headers;
+    ConstantSP params, timeout, headers;
 
     if (url->getType() != DT_STRING || url->getForm() != DF_SCALAR)
-        throw IllegalArgumentException(__FUNCTION__, "url must be a string scalar");
+        throw IllegalArgumentException(__FUNCTION__, "Url must be a string scalar");
     if (args.size() >= 2 && !args[1]->isNull()) {
         params = args[1];
-        if(
-            ((params->getForm() != DF_DICTIONARY)&&(params->getType() != DT_STRING))||
-            ((params->getForm() == DF_DICTIONARY)&&(((DictionarySP)params)->getKeyType() != DT_STRING))
-        )
-            throw IllegalArgumentException(__FUNCTION__, "params must be a string or a dictionary with keys of type string");
-    }
-    else
+        if (
+                ((params->getForm() != DF_DICTIONARY) && (params->getType() != DT_STRING)) ||
+                ((params->getForm() == DF_DICTIONARY) && (((DictionarySP) params)->getKeyType() != DT_STRING))
+                )
+            throw IllegalArgumentException(__FUNCTION__,
+                                           "Params must be a string or a dictionary with keys of type string");
+    } else
         params = new String("");
     if (args.size() >= 3) {
         timeout = args[2];
         if (!timeout->isNumber() || timeout->getForm() != DF_SCALAR)
-            throw IllegalArgumentException(__FUNCTION__, "timeout must be an integer");
-    }
-    else
+            throw IllegalArgumentException(__FUNCTION__, "Timeout must be an integer scalar");
+    } else
         timeout = new Long(0);
     if (args.size() >= 4 && !args[3]->isNull()) {
         headers = args[3];
         if ((headers->getType() != DT_STRING || headers->getForm() != DF_SCALAR) &&
             (headers->getForm() != DF_DICTIONARY ||
-            ((DictionarySP)headers)->getKeyType() != DT_STRING ||
-            ((DictionarySP)headers)->getType() != DT_STRING))
-            throw IllegalArgumentException(__FUNCTION__, "headers must be a string or a dictionary with STRING-STRING key-value type");
-    }
-    else
+             ((DictionarySP) headers)->getKeyType() != DT_STRING ||
+             ((DictionarySP) headers)->getType() != DT_STRING))
+            throw IllegalArgumentException(__FUNCTION__,
+                                           "Headers must be a string or a dictionary with STRING-STRING key-value type");
+    } else
         headers = new String("");
-    return httpClient::httpRequest(httpClient::POST, url, params, timeout,headers);
+    return httpClient::httpRequest(httpClient::POST, url, params, timeout, headers);
 }
 
 namespace httpClient {
 
-size_t curlWriteData(void *ptr, size_t size, size_t nmemb, string *data) {
-    data->append((char *)ptr, size * nmemb);
-    return size * nmemb;
-}
-
-const string getParamString(const DictionarySP &params) {
-    string paramString;
-    ConstantSP keys = params->keys();
-    for (int i = 0; i < keys->size(); i++) {
-        if (i != 0)
-            paramString += "&";
-        ConstantSP key = keys->get(i);
-        ConstantSP value = params->getMember(key);
-        paramString += key->getString();
-        paramString += "=";
-        paramString += value->getString();
+    size_t curlWriteData(void *ptr, size_t size, size_t nmemb, string *data) {
+        data->append((char *) ptr, size * nmemb);
+        return size * nmemb;
     }
-    return paramString;
-}
 
-curl_slist* setHeaders(const DictionarySP &headers,curl_slist* slist) {
-    ConstantSP keys = headers->keys();
-    for (int i = 0; i < keys->size(); i++) {
-        string strHeader;
-        ConstantSP key = keys->get(i);
-        ConstantSP value = headers->getMember(key);
-        strHeader+=key->getString();
-        strHeader+=':';
-        strHeader+= value->getString();
-        slist=curl_slist_append(slist, strHeader.c_str());
-    }
-    return slist;
-}
-
-ConstantSP httpRequest(RequestMethod method, const ConstantSP &url, const ConstantSP &params, const ConstantSP &timeout,const ConstantSP &headers) {
-    ConstantSP res = Util::createDictionary(DT_STRING, nullptr, DT_ANY, nullptr);
-    string urlString = url->getString();
-    CURL *curl = curl_easy_init();
-    if (curl) {
-        char errorBuf[CURL_ERROR_SIZE];
+    const string getParamString(const DictionarySP &params) {
         string paramString;
-
-        if (params->getForm() == DF_DICTIONARY)
-            paramString = getParamString(params);
-        else
-            paramString = params->getString();
-
-        switch(method){
-            case GET:
-                if (!paramString.empty()){
-                    urlString += "?";
-                    urlString += paramString;
-                }
-                break;
-            case POST:
-                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, paramString.c_str());
-                break;
-            default:
-                throw RuntimeException("Unknown request method");
+        ConstantSP keys = params->keys();
+        for (int i = 0; i < keys->size(); i++) {
+            if (i != 0)
+                paramString += "&";
+            ConstantSP key = keys->get(i);
+            ConstantSP value = params->getMember(key);
+            paramString += key->getString();
+            paramString += "=";
+            paramString += value->getString();
         }
+        return paramString;
+    }
 
-        curl_slist* headerList=NULL;
-        if(headers->getForm()==DF_DICTIONARY)
-            headerList=setHeaders(headers,headerList);
-        else {
-            string str_headers=headers->getString();
-            if(!str_headers.empty()){
-                int pos=str_headers.find_first_of(':');
-                int last=str_headers.find_last_of(':');
-                if(pos!=last||pos==-1||pos==0||pos==(int)str_headers.size()-1){
-                    throw IllegalArgumentException(__FUNCTION__, "String header must be like XXX:XXX");
-                }
+    curl_slist *setHeaders(const DictionarySP &headers, curl_slist *slist) {
+        ConstantSP keys = headers->keys();
+        for (int i = 0; i < keys->size(); i++) {
+            string strHeader;
+            ConstantSP key = keys->get(i);
+            ConstantSP value = headers->getMember(key);
+            strHeader += key->getString();
+            strHeader += ':';
+            strHeader += value->getString();
+            slist = curl_slist_append(slist, strHeader.c_str());
+        }
+        return slist;
+    }
+
+    ConstantSP
+    httpRequest(RequestMethod method, const ConstantSP &url, const ConstantSP &params, const ConstantSP &timeout,
+                const ConstantSP &headers) {
+        ConstantSP res = Util::createDictionary(DT_STRING, nullptr, DT_ANY, nullptr);
+        string urlString = url->getString();
+        CURL *curl = curl_easy_init();
+        if (curl) {
+            char errorBuf[CURL_ERROR_SIZE];
+            string paramString;
+
+            if (params->getForm() == DF_DICTIONARY)
+                paramString = getParamString(params);
+            else
+                paramString = params->getString();
+
+            switch (method) {
+                case GET:
+                    if (!paramString.empty()) {
+                        urlString += "?";
+                        urlString += paramString;
+                    }
+                    break;
+                case POST:
+                    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, paramString.c_str());
+                    break;
+                default:
+                    throw RuntimeException("Unknown request method");
             }
-            headerList=curl_slist_append(headerList,str_headers.c_str());
-        }
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerList);
 
-        curl_easy_setopt(curl, CURLOPT_URL, urlString.c_str());
-        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.47.0");
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
-        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuf);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, timeout->getLong());
+            curl_slist *headerList = NULL;
+            if (headers->getForm() == DF_DICTIONARY)
+                headerList = setHeaders(headers, headerList);
+            else {
+                string str_headers = headers->getString();
+                if (!str_headers.empty()) {
+                    int pos = str_headers.find_first_of(':');
+                    int last = str_headers.find_last_of(':');
+                    if (pos != last || pos == -1 || pos == 0 || pos == (int) str_headers.size() - 1) {
+                        throw IllegalArgumentException(__FUNCTION__, "If the parameter 'header' is a STRING, it must be in the format of XXX:XXX. ");
+                    }
+                }
+                headerList = curl_slist_append(headerList, str_headers.c_str());
+            }
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerList);
 
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+            curl_easy_setopt(curl, CURLOPT_URL, urlString.c_str());
+            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.47.0");
+            curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+            curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+            curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuf);
+            curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, timeout->getLong());
 
-        string responseString;
-        string headerString;
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteData);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
-        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headerString);
+            curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
 
-        CURLcode ret = curl_easy_perform(curl);
-        if (ret != 0) {
-            string errorMsg(errorBuf);
-            curl_easy_cleanup(curl);
+            string responseString;
+            string headerString;
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteData);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headerString);
+
+            CURLcode ret = curl_easy_perform(curl);
+            if (ret != 0) {
+                string errorMsg(errorBuf);
+                curl_easy_cleanup(curl);
+                curl_slist_free_all(headerList);
+                throw RuntimeException("curl returns: " + errorMsg);
+            }
+
+            long responseCode;
+            double elapsed;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
+            curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
+
+            res->set(new String("text"), new String(responseString));
+            res->set(new String("elapsed"), new Double(elapsed));
+            res->set(new String("headers"), new String(headerString));
+            res->set(new String("responseCode"), new Int(responseCode));
             curl_slist_free_all(headerList);
-            throw RuntimeException("curl returns: " + errorMsg);
+            curl_easy_cleanup(curl);
+        } else {
+            throw RuntimeException("Could not initialize request object");
         }
-
-        long responseCode;
-        double elapsed;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
-        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-
-        res->set(new String("text"), new String(responseString));
-        res->set(new String("elapsed"), new Double(elapsed));
-        res->set(new String("headers"), new String(headerString));
-        res->set(new String("responseCode"), new Int(responseCode));
-        curl_slist_free_all(headerList);
-        curl_easy_cleanup(curl);
+        return res;
     }
-    else {
-        throw RuntimeException("Could not initialize request object");
-    }
-    return res;
-}
 }
