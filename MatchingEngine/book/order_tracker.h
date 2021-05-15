b340f254ce22275e68373e04d3c5c00a5dba0cf0
xiaodanzhang <xiaodanzhang@users.noreply.github.com>
2021-05-14 20:33:44 -0400

    Add files via upload

--- MatchingEngine/book/order_tracker.h
+++ MatchingEngine/book/order_tracker.h
@@ -5,146 +5,118 @@
 
 #include "types.h"
 
-namespace book
-{
+namespace book {
 
-/// @brief Tracker of an order's state, to keep inside the OrderBook.  
+/// @brief Tracker of an order's state, to keep inside the OrderBook.
 ///   Kept separate from the order itself.
-template<typename OrderPtr>
+template <typename OrderPtr>
 class OrderTracker {
- public:
-  /// @brief construct
-  OrderTracker(const OrderPtr &order);
+public:
+    /// @brief construct
+    OrderTracker(const OrderPtr &order);
 
-  /// @brief modify the order quantity
-  void change_qty(int64_t delta);
+    /// @brief modify the order quantity
+    void change_qty(int64_t delta);
 
-  /// @brief fill an order
-  /// @param qty the number of shares filled in this fill
-  void fill(Quantity qty);
+    /// @brief fill an order
+    /// @param qty the number of shares filled in this fill
+    void fill(Quantity qty);
 
-  /// @brief is there no remaining open quantity in this order?
-  bool filled() const;
+    /// @brief is there no remaining open quantity in this order?
+    bool filled() const;
 
-  /// @brief get the total filled quantity of this order
-  Quantity filled_qty() const;
+    /// @brief get the total filled quantity of this order
+    Quantity filled_qty() const;
 
-  /// @brief get the open quantity of this order
-  Quantity open_qty() const;
+    /// @brief get the open quantity of this order
+    Quantity open_qty() const;
 
-  /// @brief get the order pointer
-  const OrderPtr &ptr() const;
+    /// @brief get the order pointer
+    const OrderPtr &ptr() const;
 
-  /// @brief get the order pointer
-  OrderPtr &ptr();
+    /// @brief get the order pointer
+    OrderPtr &ptr();
 
-  /// @ brief is this order marked all or none?
-  bool all_or_none() const;
+    /// @ brief is this order marked all or none?
+    bool all_or_none() const;
 
-  /// @ brief is this order marked immediate or cancel?
-  bool immediate_or_cancel() const;
+    /// @ brief is this order marked immediate or cancel?
+    bool immediate_or_cancel() const;
 
-  Quantity reserve(int64_t reserved);
+    Quantity reserve(int64_t reserved);
 
- private:
-  OrderPtr order_;
-  Quantity open_qty_;
-  int64_t reserved_;
-  OrderConditions conditions_;
+private:
+    OrderPtr order_;
+    Quantity open_qty_;
+    int64_t reserved_;
+    OrderConditions conditions_;
 };
 
-template<class OrderPtr>
-OrderTracker<OrderPtr>::OrderTracker(const OrderPtr &order)
-  : order_(order),
-    open_qty_(order->order_qty()),
-    reserved_(0),
-    conditions_(0)
-{
-  if (order->all_or_none()) {
-    conditions_ |= oc_all_or_none;
-  }
-  if (order->immediate_or_cancel()) {
-    conditions_ |= oc_immediate_or_cancel;
-  }
+template <class OrderPtr>
+OrderTracker<OrderPtr>::OrderTracker(const OrderPtr &order) : order_(order), open_qty_(order->order_qty()), reserved_(0), conditions_(0) {
+    if (order->all_or_none()) {
+        conditions_ |= oc_all_or_none;
+    }
+    if (order->immediate_or_cancel()) {
+        conditions_ |= oc_immediate_or_cancel;
+    }
 }
 
-template<class OrderPtr>
-Quantity
-OrderTracker<OrderPtr>::reserve(int64_t reserved)
-{
-  reserved_ += reserved;
-  return open_qty_ - reserved_;
+template <class OrderPtr>
+Quantity OrderTracker<OrderPtr>::reserve(int64_t reserved) {
+    reserved_ += reserved;
+    return open_qty_ - reserved_;
 }
 
-template<class OrderPtr>
-void
-OrderTracker<OrderPtr>::change_qty(int64_t delta)
-{
-  if ((delta < 0 &&
-    (int) open_qty_ < std::abs(delta))) {
-    throw
-      std::runtime_error("Replace size reduction larger than open quantity");
-  }
-  open_qty_ += delta;
+template <class OrderPtr>
+void OrderTracker<OrderPtr>::change_qty(int64_t delta) {
+    if ((delta < 0 && (int)open_qty_ < std::abs(delta))) {
+        throw std::runtime_error("Replace size reduction larger than open quantity");
+    }
+    open_qty_ += delta;
 }
 
-template<class OrderPtr>
-void
-OrderTracker<OrderPtr>::fill(Quantity qty)
-{
-  if (qty > open_qty_) {
-    throw std::runtime_error("Fill size larger than open quantity");
-  }
-  open_qty_ -= qty;
+template <class OrderPtr>
+void OrderTracker<OrderPtr>::fill(Quantity qty) {
+    if (qty > open_qty_) {
+        throw std::runtime_error("Fill size larger than open quantity");
+    }
+    open_qty_ -= qty;
 }
 
-template<class OrderPtr>
-bool
-OrderTracker<OrderPtr>::filled() const
-{
-  return open_qty_ == 0;
+template <class OrderPtr>
+bool OrderTracker<OrderPtr>::filled() const {
+    return open_qty_ == 0;
 }
 
-template<class OrderPtr>
-Quantity
-OrderTracker<OrderPtr>::filled_qty() const
-{
-  return order_->order_qty() - open_qty();
+template <class OrderPtr>
+Quantity OrderTracker<OrderPtr>::filled_qty() const {
+    return order_->order_qty() - open_qty();
 }
 
-template<class OrderPtr>
-Quantity
-OrderTracker<OrderPtr>::open_qty() const
-{
-  return open_qty_ - reserved_;
+template <class OrderPtr>
+Quantity OrderTracker<OrderPtr>::open_qty() const {
+    return open_qty_ - reserved_;
 }
 
-template<class OrderPtr>
-const OrderPtr &
-OrderTracker<OrderPtr>::ptr() const
-{
-  return order_;
+template <class OrderPtr>
+const OrderPtr &OrderTracker<OrderPtr>::ptr() const {
+    return order_;
 }
 
-template<class OrderPtr>
-OrderPtr &
-OrderTracker<OrderPtr>::ptr()
-{
-  return order_;
+template <class OrderPtr>
+OrderPtr &OrderTracker<OrderPtr>::ptr() {
+    return order_;
 }
 
-template<class OrderPtr>
-bool
-OrderTracker<OrderPtr>::all_or_none() const
-{
-  return bool(conditions_ & oc_all_or_none);
+template <class OrderPtr>
+bool OrderTracker<OrderPtr>::all_or_none() const {
+    return bool(conditions_ & oc_all_or_none);
 }
 
-template<class OrderPtr>
-bool
-OrderTracker<OrderPtr>::immediate_or_cancel() const
-{
-  return bool((conditions_ & oc_immediate_or_cancel) != 0);
+template <class OrderPtr>
+bool OrderTracker<OrderPtr>::immediate_or_cancel() const {
+    return bool((conditions_ & oc_immediate_or_cancel) != 0);
 }
 
-}
+}    // namespace book
