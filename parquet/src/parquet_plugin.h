afd71c64d01bae104f1a8f222cc6656d2acdde4d
xzhang <it@dolphindb.com>
2021-09-08 17:47:56 -0400

    fixed parquet loading data error

--- parquet/src/parquet_plugin.h
+++ parquet/src/parquet_plugin.h
@@ -20,10 +20,14 @@
 #include <parquet/api/schema.h>
 #include <parquet/api/reader.h>
 #include <parquet/api/writer.h>
+#include <arrow/io/file.h>
 extern "C" ConstantSP extractParquetSchema(const ConstantSP &filename);
 extern "C" ConstantSP loadParquet(Heap *heap, vector<ConstantSP> &arguments);
+extern "C" ConstantSP loadParquetHdfs(Heap *heap, vector<ConstantSP> &arguments);
 extern "C" ConstantSP loadParquetEx(Heap *heap, vector<ConstantSP> &arguments);
 extern "C" ConstantSP parquetDS(Heap *heap, vector<ConstantSP>& arguments);
+extern "C" ConstantSP saveParquet(Heap *heap, vector<ConstantSP>& arguments);
+extern "C" ConstantSP saveParquetHdfs(Heap *heap, vector<ConstantSP>& arguments);
 
 namespace ParquetPluginImp
 {
@@ -39,6 +43,7 @@ class ParquetReadOnlyFile
     ParquetReadOnlyFile(const ParquetReadOnlyFile &rhs) = delete;
     ParquetReadOnlyFile &operator=(const ParquetReadOnlyFile &rhs) = delete;
     void open(const std::string &file_name);
+    void open(std::shared_ptr<arrow::io::RandomAccessFile> source);
     void close();
     std::shared_ptr<parquet::FileMetaData> fileMetadataReader(){return _parquet_reader->metadata();}
     std::shared_ptr<parquet::RowGroupReader> rowReader(int i);
@@ -65,27 +70,53 @@ bool getSchemaCol(const parquet::SchemaDescriptor *schema_descr,const ConstantSP
 void createNewVectorSP(vector<VectorSP> &dolpindb_v,const TableSP &tb);
 
 TableSP appendColumnVecToTable(TableSP tb, vector<VectorSP> &colVec);
-bool convertToDTdate(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTminute(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTmonth(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTdatetime(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTtime(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTtimestamp(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTsecond(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTnanotimestamp(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
-bool convertToDTnanotime(vector<int> &intValue, parquetTime times_t,vector<long long> &longValue);
+
 parquetTime convertParquetTimes(const std::shared_ptr<const parquet::LogicalType>& logical_t, parquet::ConvertedType::type converted_t);
-bool convertParquetToDolphindbChar(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<char> &buffer);
-bool convertParquetToDolphindbBool(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<char> &buffer);
-bool convertParquetToDolphindbInt(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<int> &buffer,DATA_TYPE times_t);
-bool convertParquetToDolphindbShort(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<short> &buffer);
-bool convertParquetToDolphindbLong(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<long long> &buffer,DATA_TYPE times_t);
-bool convertParquetToDolphindbFloat(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<float> &buffer);
-bool convertParquetToDolphindbDouble(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader,const parquet::ColumnDescriptor *col_descr,vector<double> &buffer );
-bool convertParquetToDolphindbString(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<string> &buffer, DATA_TYPE string_t);
+
+int convertParquetToDolphindbChar(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr, char* buffer, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbBool(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr, char* buffer, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbInt(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr, int* buffer,DATA_TYPE times_t, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbShort(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,short* buffer, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbLong(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,long long* buffer,DATA_TYPE times_t, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbFloat(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,float* buffer, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbDouble(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader,const parquet::ColumnDescriptor *col_descr,double *buffer, size_t batchSize, bool& containNull);
+
+int convertParquetToDolphindbString(int col_idx,std::shared_ptr<parquet::ColumnReader> column_reader, const parquet::ColumnDescriptor *col_descr,vector<string> &buffer, DATA_TYPE string_t, size_t batchSize, bool& containNull);
+
 ConstantSP loadParquet(const string &filename, const ConstantSP &schema, const ConstantSP &column, const int rowGroupStart, const int rowGroupNum);
+
 ConstantSP loadParquet(ParquetReadOnlyFile*file, const ConstantSP &schema, const ConstantSP &column, const int rowGroupStart, const int rowGroupEnd);
+
+ConstantSP loadParquetHdfs(void *buffer, int64_t len);
+
 ConstantSP parquetDS(const ConstantSP &filename,const ConstantSP &schema);
+
+ConstantSP saveParquet(ConstantSP &tb, const string &filename);
+
+ConstantSP saveParquetHdfs(ConstantSP &table);
+
+std::shared_ptr<parquet::schema::GroupNode> getParquetSchemaFromDolphindb(const TableSP &table);
+
+void writeBoolToParquet(parquet::BoolWriter *parquetCol, const VectorSP &dolphinCol);
+
+void writeIntToParquet(parquet::Int32Writer *parquetCol, const VectorSP &dolphinCol, int (*transform)(int) = [](int v){return v;});
+
+void writeLongToParquet(parquet::Int64Writer *parquetCol, const VectorSP &dolphinCol, long long (*transform)(long long) = [](long long v){return v;});
+
+void writeFloatToParquet(parquet::FloatWriter *parquetCol, const VectorSP &dolphinCol);
+
+void writeDoubleToParquet(parquet::DoubleWriter *parquetCol, const VectorSP &dolphinCol);
+
+// void writeCharToParquet(parquet::FixedLenByteArrayWriter *parquetCol, const VectorSP &dolphinCol);
+
+void writeStringToParquet(parquet::ByteArrayWriter *parquetCol, const VectorSP &dolphinCol);
+
 //ConstantSP loadFromParquetToDatabase(Heap *heap, vector<ConstantSP> &arguments);
 vector<DistributedCallSP> generateParquetTasks(Heap* heap, ParquetReadOnlyFile *file, const TableSP &schema, const ConstantSP &column,
                                           const int rowGroupStart, const int rowGroupNum, const SystemHandleSP &db, const string &tableName, const ConstantSP &transform);
@@ -95,9 +126,14 @@ TableSP generateInMemoryParitionedTable(Heap *heap, const SystemHandleSP &db,
 
 ConstantSP loadParquetEx(Heap *heap, const SystemHandleSP &db, const string &tableName, const ConstantSP &partitionColumns,
                          const string &filename, const TableSP &schema, const ConstantSP &column, const int rowGroupStart, const int rowGroupNum, const ConstantSP &transform);
+
 int getRowGroup(const string &filename);
+
 void getParquetReadOnlyFile(Heap *heap, vector<ConstantSP> &arguments);
-bool isDecimal(const parquet::ColumnDescriptor *col_descr);
+
+inline bool isDecimal(const parquet::ColumnDescriptor *col_descr){
+  return col_descr->logical_type()->is_decimal() || (col_descr->converted_type()==parquet::ConvertedType::DECIMAL);
+}
 }
 
 #endif
