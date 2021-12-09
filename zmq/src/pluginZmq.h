e27784ec380aaf29fae9c5f6ca777ad892befc1f
hzwale <xiaojun.qian@dolphindb.com>
2021-12-09 17:14:12 +0800

    add zmq

---
+++ zmq/src/pluginZmq.h
@@ -0,0 +1,247 @@
+#ifndef PLUGINZMQ_PLUGINZMQ_H
+#define PLUGINZMQ_PLUGINZMQ_H
+
+#include "Concurrent.h"
+#include "ScalarImp.h"
+#include "zmq.h"
+#include "zmq.hpp"
+#include "ConstantMarshall.h"
+#include "json.hpp"
+#include "Logger.h"
+
+using namespace std;
+using json = nlohmann::json;
+
+extern "C" ConstantSP zmqSocket(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqSend(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqConnect(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqBind(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqClose(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqCreateSubJob(Heap *heap, vector<ConstantSP> &args);
+
+extern "C" ConstantSP zmqCancelSubJob(Heap *heap, vector<ConstantSP> args);
+
+extern "C" ConstantSP zmqGetSubJobStat(Heap *heap, vector<ConstantSP> &args);
+
+static shared_ptr<zmq::socket_t> createZmqSocket(zmq::context_t &context, const string &socketType) {
+    if (socketType == "ZMQ_PUB") {
+        return shared_ptr<zmq::socket_t>(new zmq::socket_t(context, ZMQ_PUB));
+    } else if (socketType == "ZMQ_SUB") {
+        return shared_ptr<zmq::socket_t>(new zmq::socket_t(context, ZMQ_SUB));
+    } else if (socketType == "ZMQ_PUSH") {
+        return shared_ptr<zmq::socket_t>(new zmq::socket_t(context, ZMQ_PUSH));
+    } else if (socketType == "ZMQ_PULL") {
+        return shared_ptr<zmq::socket_t>(new zmq::socket_t(context, ZMQ_PULL));
+    } else {
+        throw RuntimeException("the zmq socket type is not supported");
+    }
+}
+
+class ZmqSocket {
+public:
+    ZmqSocket(const string &type)
+            : context(), zmq_Socket_(createZmqSocket(context, type)) {}
+
+    void connect(const string &addr, const string &prefix) {
+        zmq_Socket_->connect(addr);
+        addr_ = addr;
+        prefix_ = prefix;
+    }
+
+    void bind(const string addr, const string &prefix) {
+        addr_ = addr;
+        zmq_Socket_->bind(addr);
+        prefix_ = prefix;
+    }
+
+    string str() {
+        return addr_;
+    }
+
+    ~ZmqSocket() {}
+
+    shared_ptr<zmq::socket_t> getSocket() {
+        return zmq_Socket_;
+    }
+
+    string getPrefix() {
+        return prefix_;
+    }
+
+    string getAddr() {
+        return addr_;
+    }
+
+    string getType() {
+        return type_;
+    }
+
+protected:
+    zmq::context_t context;
+    shared_ptr<zmq::socket_t> zmq_Socket_;
+    string prefix_;
+    string addr_;
+    string type_;
+};
+
+class ZmqPushSocket : public ZmqSocket {
+public:
+    ZmqPushSocket(const string &type, FunctionDefSP formatter, int batchSize) : ZmqSocket(type), formatter_(formatter), batchSize_(batchSize){}
+
+    FunctionDefSP getFormatter() {
+        return formatter_;
+    }
+
+    int getBatchSize(){
+        return batchSize_;
+    }
+
+protected:
+    FunctionDefSP formatter_;
+    int batchSize_;
+};
+
+class ZmqSubSocket : public ZmqSocket {
+public:
+    ZmqSubSocket(const string &addr, const string &type, FunctionDefSP parser, const string &prefix, bool isConnect) :
+            ZmqSocket(type), parser_(parser) {
+        if(isConnect)
+            connect(addr, prefix);
+        else
+            bind(addr, prefix);
+        if(type == "ZMQ_SUB")
+            zmq_Socket_->setsockopt(ZMQ_SUBSCRIBE, prefix.c_str(), prefix.size());
+        zmq_Socket_->setsockopt(ZMQ_RCVTIMEO, 100);
+    }
+
+    FunctionDefSP getParser() {
+        return parser_;
+    }
+
+    void close(){
+        zmq_Socket_->close();
+    }
+
+protected:
+    FunctionDefSP parser_;
+};
+
+class DummyOutput : public Output {
+public:
+    virtual bool timeElapsed(long long nanoSeconds) { return true; }
+
+    virtual bool write(const ConstantSP &obj) { return true; }
+
+    virtual bool message(const string &msg) { return true; }
+
+    virtual void enableIntermediateMessage(bool enabled) {}
+
+    virtual IO_ERR done() { return OK; }
+
+    virtual IO_ERR done(const string &errMsg) { return OK; }
+
+    virtual bool start() { return true; }
+
+    virtual bool start(const string &message) { return true; }
+
+    virtual IO_ERR writeReady() { return OK; }
+
+    virtual ~DummyOutput() {}
+
+    virtual OUTPUT_TYPE getOutputType() const { return STDOUT; }
+
+    virtual void close() {}
+
+    virtual void setWindow(INDEX index, INDEX size) {};
+
+    virtual IO_ERR flush() { return OK; }
+};
+
+
+class AppendTable : public Runnable {
+public:
+    AppendTable(Heap *heap, shared_ptr<ZmqSubSocket> socket, const FunctionDefSP &parser, ConstantSP handle)
+            : socket_(socket), parser_(parser), handle_(handle), recv(0), needStop_(false), isStop_(1) {
+        session_ = heap->currentSession()->copy();
+        session_->setUser(heap->currentSession()->getUser());
+        session_->setOutput(new DummyOutput);
+        isStop_.acquire();
+    }
+
+    void run() override;
+
+    shared_ptr<ZmqSubSocket> getZmqSocket() {
+        return socket_;
+    }
+
+    void increaseRecv() {
+        recv.fetch_add(1);
+    }
+
+    long long getRecv() {
+        return recv;
+    }
+
+    void setNeedStop(){
+        needStop_.store(true);
+    }
+
+    void cancelThread(){
+        needStop_.store(true);
+        isStop_.acquire();
+        socket_->close();
+    }
+
+private:
+    shared_ptr<ZmqSubSocket> socket_;
+    FunctionDefSP parser_;
+    ConstantSP handle_;
+    SessionSP session_;
+    atomic<long long> recv;
+    atomic<bool> needStop_;
+    Semaphore isStop_;
+};
+
+class SubConnection {
+private:
+    long long createTime_;
+    Heap *heap_;
+    ThreadSP thread_;
+    SessionSP session_;
+    SmartPointer<AppendTable> appendTable_;
+public:
+
+    SubConnection(Heap *heap, shared_ptr<ZmqSubSocket> socket, const FunctionDefSP &parser, ConstantSP handle);
+
+    long long getCreateTime() const {
+        return createTime_;
+    }
+
+    SessionSP getSession() {
+        return session_;
+    }
+
+    SmartPointer<AppendTable> getAppendTable() {
+        return appendTable_;
+    }
+
+    Heap *getHeap() {
+        return heap_;
+    }
+
+    void cancelThread() {
+        appendTable_->cancelThread();
+    }
+
+    ~SubConnection(){
+        appendTable_->setNeedStop();
+        thread_->join();
+    }
+};
+
+#endif //PLUGINZMQ_PLUGINZMQ_H
