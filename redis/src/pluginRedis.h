e777cf9f2787f0a6648994a7905d953f77d099b0
Jinzhi Chen <jinzhi.chen@dolphindb.com>
2024-04-03 14:21:09 +0800

    update redis to release200.12

--- redis/src/pluginRedis.h
+++ redis/src/pluginRedis.h
@@ -2,13 +2,15 @@
 #define PLUGIN_REDIS_H
 
 #include "CoreConcept.h"
+#include "ddbplugin/CommonInterface.h"
 
-extern "C" ConstantSP redisPluginConnect(Heap *heap, const vector<ConstantSP>& args);
-extern "C" ConstantSP redisPluginRun(Heap *heap, const vector<ConstantSP>& args);
-extern "C" ConstantSP redisPluginBatchSet(Heap *heap, const vector<ConstantSP>& args);
-extern "C" ConstantSP redisPluginRelease(const ConstantSP& handle);
+extern "C" ConstantSP redisPluginConnect(Heap *heap, const vector<ConstantSP> &args);
+extern "C" ConstantSP redisPluginRun(Heap *heap, const vector<ConstantSP> &args);
+extern "C" ConstantSP redisPluginBatchSet(Heap *heap, const vector<ConstantSP> &args);
+extern "C" ConstantSP redisPluginBatchHashSet(Heap *heap, const vector<ConstantSP> &args);
+extern "C" ConstantSP redisPluginRelease(const ConstantSP &handle);
 extern "C" ConstantSP redisPluginReleaseAll();
-extern "C" ConstantSP redisGetHandle(Heap *heap, const vector<ConstantSP>& args);
+extern "C" ConstantSP redisGetHandle(Heap *heap, const vector<ConstantSP> &args);
 extern "C" ConstantSP redisGetHandleStaus();
 
 #endif
